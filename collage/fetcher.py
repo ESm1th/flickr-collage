01800f3d88148055f363bf886ad605078ed9b391
Evgeny <ku7nec0v@gmail.com>
2020-02-14 10:56:39 +0300

    refactor: docstrings and annotations

--- collage/fetcher.py
+++ collage/fetcher.py
@@ -20,17 +20,30 @@ class ImageFetcher:
     }
     methods = SimpleNamespace(**METHODS)
 
-    def __init__(self, api_key: str, search_text: str, quantity: int = 100):
+    def __init__(
+        self,
+        api_key: str,
+        search_text: str,
+        quantity: int
+    ) -> None:
         self.params.update({'api_key': api_key, 'per_page': quantity})
         self.search_text = search_text
         self.quantity = quantity
 
-    async def make_request(self, payload: dict, format='json'):
+    async def make_request(self, payload: dict, format='json') -> dict:
+        """
+        Send request with passed payload and return `json` data of response.
+        """
         async with request('get', self.endpoint, params=payload) as response:
             data = await asyncio.create_task(response.json())
             return data
 
-    async def search_photos(self, urls_queue):
+    async def search_photos(self, urls_queue: 'AutoProxy[Queue]') -> None:
+        """
+        Create and send request to get photos from `flickr` with specified
+        parameters. Process received photos and create tasks to fetch urls for
+        each photo.
+        """
         payload = copy(self.params)
         payload.update(
             {
@@ -42,15 +55,27 @@ class ImageFetcher:
         photos = data['photos']['photo']
         photos_ids = [
             photo['id'] for photo in photos
-            if photo['ispublic'] == 1
+            if photo['ispublic'] == 1  # only public images
         ]
         await asyncio.create_task(self.get_photos_urls(photos_ids, urls_queue))
 
-    async def get_photo_sizes(self, payload, urls_queue):
+    async def get_photo_sizes(
+        self, payload: dict, urls_queue: 'asyncio.queues.Queue'
+    ) -> None:
+        """
+        Send request to get response with photo sizes and put `json` data
+        of this request to queue for further processing.
+        """
         response_data = await asyncio.create_task(self.make_request(payload))
         await asyncio.create_task(urls_queue.put(response_data))
 
-    async def get_photos_urls(self, photos_ids, urls_queue):
+    async def get_photos_urls(
+        self, photos_ids: list, urls_queue: 'asyncio.queues.Queue'
+    ) -> None:
+        """
+        For each photo in `photo_ids` argument create task to send request
+        with appropriate payload.
+        """
         tasks = []
 
         for photo_id in photos_ids:
@@ -69,40 +94,42 @@ class ImageFetcher:
         await asyncio.gather(*tasks)
         await asyncio.create_task(urls_queue.put(None))
 
-    async def get_photo(self, url, image_queue):
+    async def get_photo(
+        self, url: str, image_queue: 'AutoProxy[Queue]'
+    ) -> None:
+        """
+        Send request to get response with image and put received image to
+        `image_queue` as bytes.
+        """
         async with request('get', url) as response:
             image = await asyncio.create_task(response.read())
-            await asyncio.create_task(image_queue.put(image))
+            image_queue.put(image)
 
-    async def download_photos(self, urls_queue, image_queue):
+    async def download_photos(
+        self, urls_queue: 'asyncio.queues.Queue',
+        image_queue: 'AutoProxy[Queue]'
+    ) -> None:
+        """
+        Get photo data from `urls_queue`, parse it and create task to download
+        photo file.
+        """
         while True:
             photo = await asyncio.create_task(urls_queue.get())
 
             if photo is None:
-                await asyncio.create_task(image_queue.put(None))
+                image_queue.put(None)
                 break
 
             url = photo['sizes']['size'][-1]['source']
             task = asyncio.create_task(self.get_photo(url, image_queue))
             await task
 
-    async def fetch(self):
+    async def fetch(self, images_queue: 'AutoProxy[Queue]'):
+        """
+        Main method to run in `asyncio.run`. Create two main tasks: one for
+        searching and getting urls for downloading images and second - to check
+        queue with urls for downloading images and use them to download images.
+        """
         urls = asyncio.Queue()
-        images = asyncio.Queue()
         await asyncio.create_task(self.search_photos(urls))
-        await asyncio.create_task(self.download_photos(urls, images))
-
-
-if __name__ == '__main__':
-    from time import time
-
-    start = time()
-    import os
-    from dotenv import load_dotenv
-    load_dotenv()
-
-    api_key = os.environ['API_KEY']
-    fetcher = ImageFetcher(api_key, 'love', quantity=20)
-    asyncio.run(fetcher.fetch())
-    end = time()
-    print(end - start)
+        await asyncio.create_task(self.download_photos(urls, images_queue))
